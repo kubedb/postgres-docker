ce25d9adb019a6fa8c138079c29b24c8a524fa54
Emon46 <emon@appscode.com>
2021-03-08 02:15:20 +0600

    fix: added version 11.10 fix: added ssl support fix: added client auth mode cert
    Signed-off-by: Emon46 <emon@appscode.com>

---
+++ scripts/config_recovery.conf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eou pipefail
+
+
+touch /tmp/recovery.conf
+echo "standby_mode = on" >>/tmp/recovery.conf
+echo "trigger_file = '/run_scripts/tmp/pg-failover-trigger'" >>/tmp/recovery.conf
+
+#echo "archive_cleanup_command = 'pg_archivecleanup $PGWAL %r'" >>/tmp/recovery.conf
+echo "recovery_target_timeline = 'latest'" >>/tmp/recovery.conf
+
+# primary_conninfo is used for streaming replication
+if [[ "${SSL:-0}" == "ON" ]]; then
+  if [[ "$CLIENT_AUTH_MODE" == "cert" ]]; then
+       echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST password=$POSTGRES_PASSWORD sslmode=$SSL_MODE sslrootcert=/tls/certs/client/ca.crt sslcert=/tls/certs/client/client.crt sslkey=/tls/certs/client/client.key'" >>/tmp/recovery.conf
+   else
+       echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST password=$POSTGRES_PASSWORD sslmode=$SSL_MODE sslrootcert=/tls/certs/client/ca.crt'" >>/tmp/recovery.conf
+   fi
+else
+   echo "primary_conninfo = 'application_name=$HOSTNAME host=$PRIMARY_HOST password=$POSTGRES_PASSWORD'" >>/tmp/recovery.conf
+fi
+
+
+mv /tmp/recovery.conf "$PGDATA/recovery.conf"
---
+++ scripts/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eou pipefail
+#going to change this with the check of process id
+rm -f "$PGDATA"/postmaster.pid
+echo "waiting for the role to be decided ..."
+while true; do
+    if [[ -e /run_scripts/role/run.sh ]]; then
+        echo "running the initial script ..."
+        /run_scripts/role/run.sh && break ;
+    fi
+    sleep 1
+done
